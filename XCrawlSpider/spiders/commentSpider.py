7a42fede006d5015be79b4dae2a651c7414184f5
4HET <2659335014@qq.com>
2022-11-07 13:37:40 +0800

    做了部分异常处理

--- XCrawlSpider/spiders/commentSpider.py
+++ XCrawlSpider/spiders/commentSpider.py
@@ -23,30 +23,37 @@ class CommentspiderSpider(scrapy.Spider):
         for url in self.start_urls:
             self.shop_id = url.split('/')[-1]
             headers = {
-                'Cookie': 'fspop=test; cy=153; cye=rizhao; _lxsdk_cuid=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _lxsdk=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _hc.v=7db62757-4617-03c5-8227-a3b30a9803c6.1666510368; s_ViewType=10; WEBDFPID=5w0830xz08w35yvu197y7429u76xy6u781596x519x997958zvww6ww3-1981870520158-1666510519831MQUYSAUfd79fef3d01d5e9aadc18ccd4d0c95077639; dper=ab7eef6129a412b25b0ac3854ff9e3bd0bf35371e8df4c10679b2f4ae332c4520bd2d188510101887353e78941e52b4a8a57ffa532d2ed4c662b55acd1dda1b993627620ed1e01619351ee224709f7c84571b76e4d9481e5692247f0803176e1; ua=dpuser_3830243167; ctu=3eada7613bfd5549da00debc4ee9ff6190908a18a5ec7383aee478c9ff16b664; _lx_utm=utm_source=bing&utm_medium=organic; ll=7fd06e815b796be3df069dec7836c3df; dplet=a1ded827ad67a2f1eaf855c2d2249de0; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1666950926,1666995148,1667001764,1667098148; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1667099170; _lxsdk_s=18426cafb66-ae0-e72-2ab||121',
+                'Cookie': '_lxsdk_cuid=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _lxsdk=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _hc.v=7db62757-4617-03c5-8227-a3b30a9803c6.1666510368; s_ViewType=10; WEBDFPID=5w0830xz08w35yvu197y7429u76xy6u781596x519x997958zvww6ww3-1981870520158-1666510519831MQUYSAUfd79fef3d01d5e9aadc18ccd4d0c95077639; ctu=3eada7613bfd5549da00debc4ee9ff6190908a18a5ec7383aee478c9ff16b664; cy=1946; cye=bange; dper=de89a983a903d8800d8406b5e096b455290424d5cd5b889f9d0313532957c54f62516b79d73044ac32008348c20522b285b6d3a3a37fcca240e3362fe3d1ded7; ll=7fd06e815b796be3df069dec7836c3df; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1667380665,1667492509,1667796220,1667797032; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1667797079; _lxsdk_s=1845066b67b-9cb-592-a2b%7C%7C88',
                 'User-Agent': UserAgent().random,
                 'Host': 'www.dianping.com',
                 'Referer': 'https://www.dianping.com/shop/{}'.format(self.shop_id),
             }
-            url = 'https://www.dianping.com/ajax/json/shopDynamic/allReview?shopId={}&cityId=8&shopType=10&tcv=4e7hqrxmfg&_token={}&uuid=7db62757-4617-03c5-8227-a3b30a9803c6.1666510368&platform=1&partner=150&optimusCode=10&originUrl=https://www.dianping.com/shop/l2g0kxUJRpV1bdZr'.format(
-                self.shop_id, self.get_token())
-            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
+            url = f'https://www.dianping.com/ajax/json/shopDynamic/allReview?shopId={self.shop_id}&cityId=8&shopType=10&tcv=4e7hqrxmfg&_token={self.get_token()}&uuid=7db62757-4617-03c5-8227-a3b30a9803c6.1666510368&platform=1&partner=150&optimusCode=10&originUrl=https://www.dianping.com/shop/l2g0kxUJRpV1bdZr'
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse, meta={'url': url, 'headers': headers})
 
     def parse(self, response):
         data = json.loads(response.text)
-        print(data.keys())
-        # print(response.url)
-        # print(response.headers)
-        # rs = json.loads(response.text)
-        # item = response.meta['item']
-        # data = item
-        # print(response)
-        # print("paser")
+        # print(data.keys())
+        # print(data)
+        code = data['code']
+
+        if code != 200:
+            url = response.meta['url']
+            headers = response.meta['headers']
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse, meta={'url': url, 'headers': headers})
+        else:
+            reviewAllDOList = data['reviewAllDOList']
+            for reviewDataVO in reviewAllDOList:
+                # 评论体
+                print(reviewDataVO['reviewDataVO']['reviewData']['reviewBody'])
+            # print(reviewAllDOList)
+            with open('data.json', 'w', encoding='utf-8') as fp:
+                json.dump(data, fp, ensure_ascii=False)
 
     def get_token(self):
         with open('./js/rohr.min.js', 'r', encoding='utf-8') as fp:
             js = fp.read()
 
         JS = execjs.compile(js)
-        token = JS.call("hhh_token", 'https://www.dianping.com?shopId={}'.format(self.shop_id))
+        token = JS.call("hhh_token", f'https://www.dianping.com?shopId={self.shop_id}')
         return token
