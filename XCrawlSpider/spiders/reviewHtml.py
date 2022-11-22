36c3f038c9cd17dee5262f9ce0dce5779a70f103
4HET <2659335014@qq.com>
2022-11-22 20:52:57 +0800

    修复了令人伤心的bug

--- XCrawlSpider/spiders/reviewHtml.py
+++ XCrawlSpider/spiders/reviewHtml.py
@@ -8,18 +8,29 @@ import pandas as pd
 from fake_useragent import UserAgent
 import re
 
+from lxml import etree
+
+
 class ReviewhtmlSpider(scrapy.Spider):
     name = 'reviewHtml'
     # allowed_domains = ['xxx.com']
     # start_urls = ['http://xxx.com/']
 
     def start_requests(self):
+        fp = open('./css/background.css', 'r', encoding='utf-8')
+
+        resp = fp.read()
+        pattern = re.compile(r'\.(?P<tag>.*?)\{background:-(?P<x>.*?)px-(?P<y>.*?)px')
+        self.result = pattern.finditer(resp)
+        self.background = 'http:' + 'http://s3plus.meituan.net/v1/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss/c52c5f54f0f55645b6995aa1db09182b.svg'
+        # self.background = 'http:' + re.findall(r'background-image:url(.*?)', resp)[0]
+
         # 读取json
         with open('./json/city_list.json', 'r', encoding='utf-8') as fp:
             data = json.load(fp)
 
-        with open('XCrawlSpider/spiders/json/review.json', 'r', encoding='utf-8') as fp:
-            self.review = json.load(fp)
+        # with open('XCrawlSpider/spiders/json/review.json', 'r', encoding='utf-8') as fp:
+        #     self.review = json.load(fp)
 
         city_list = data.keys()
         # print(city_list)
@@ -29,6 +40,10 @@ class ReviewhtmlSpider(scrapy.Spider):
         city_log = pd.read_csv(path).iloc[:, 0].values
         log = pd.read_csv(path).iloc[:, 1].values
         log = pd.DataFrame(log, columns=['shop_url'])
+
+
+        result = pattern.findall(resp)
+        print(result)
         # print(city_list)
         for city in city_list:
             pds = pd.read_csv(f'./csv/{city}.csv').iloc[:, 1].values
@@ -44,7 +59,6 @@ class ReviewhtmlSpider(scrapy.Spider):
             # if city not in city_log:
             cy = 57
             cye = 'alashan'
-            print(pds)
             for urls in pds.iloc[:, 0].values:
                 url = urls + '/review_all'
                 # print(self.shop_id)
@@ -52,37 +66,27 @@ class ReviewhtmlSpider(scrapy.Spider):
                     # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
                     'User-Agent': UserAgent().random,
                     'Host': 'www.dianping.com',
-                    # 'Referer': 'https://www.dianping.com/shop/{}'.format(self.shop_id),
-                    'Cookie': '_lxsdk_cuid=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _lxsdk=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _hc.v=9fec2d1c-9253-9f08-843d-4d8d0bd47aaa.1668095623; s_ViewType=10; fspop=test; __utma=205923334.1124010926.1668848108.1668848108.1668848108.1; __utmz=205923334.1668848108.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); WEBDFPID=8x7y7yz136w6589yz97u5zwy8z6x294v8151u8y12zx979588w9915xu-1984208257365-1668848257101QMIMWGKfd79fef3d01d5e9aadc18ccd4d0c95071406; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1668911845,1668918822,1668945215,1669003562; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; cy=4; cye=guangzhou; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1669022595; _lxsdk_s=18499718bf4-8d5-61-8e9%7C%7C491'
+                    'Cookie': '_lxsdk_cuid=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _lxsdk=18403c22a69c8-0cbd5899ee277f-7b555472-384000-18403c22a69c8; _hc.v=9fec2d1c-9253-9f08-843d-4d8d0bd47aaa.1668095623; s_ViewType=10; fspop=test; __utma=205923334.1124010926.1668848108.1668848108.1668848108.1; __utmz=205923334.1668848108.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); WEBDFPID=8x7y7yz136w6589yz97u5zwy8z6x294v8151u8y12zx979588w9915xu-1984208257365-1668848257101QMIMWGKfd79fef3d01d5e9aadc18ccd4d0c95071406; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; cy=4; cye=guangzhou; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1668918822,1668945215,1669003562,1669080037; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1669082774; _lxsdk_s=1849cec2e9e-6f7-26d-0f5%7C%7C414'
                 }
-                print(f"*******step 1:********\nurl:\t{url}")
-                time.sleep(random.randint(1, 3))
-                time.sleep(random.randint(1, 3))
-                yield scrapy.Request(url=url, headers=headers, callback=self.parse_css,
+                print(f"step 1:\nurl:\t{url}")
+                time.sleep(random.randint(1, 6))
+                time.sleep(random.randint(1, 6))
+                yield scrapy.Request(url='https://www.baidu.com', headers=headers, callback=self.parse_css,
                                      meta={'url': url, 'headers': headers, 'city': city},
                                      dont_filter=True)
                 # yield scrapy.http.Request(url=url, headers=headers, callback=self.parse, meta={'url': url, 'headers': headers})
 
     def parse_css(self, response):
-        fp = open('./index.html', 'w', encoding='utf-8')
-        resp = response.text
-        print(resp)
-        # 正则表达式
-        pattern = re.compile(r'<link rel="stylesheet" type="text/css" href="(.*?)">')
-        # 匹配
-        css_url = pattern.findall(resp)
-        print(css_url)
+        print(f"step 2:\nurl:\t{response.url}")
 
-        # print到文件
-        print(resp, file=fp)
         # data = json.loads(response.text)
         # print(data.keys())
         # print(data)
-
-        from XCrawlSpider.items import XcrawlspiderItem
-        item = XcrawlspiderItem()
-        # city = response.meta['city']
-        mid_re = []
+        #
+        # from XCrawlSpider.items import XcrawlspiderItem
+        # item = XcrawlspiderItem()
+        # # city = response.meta['city']
+        # mid_re = []
         # item['html'] = resp
         # # 评论
         # item['review'] = mid_re
@@ -99,6 +103,11 @@ class ReviewhtmlSpider(scrapy.Spider):
         #                      meta={'url': css_url, 'headers': headers, 'city': response.meta['city'], 'shop_url': response.meta['url'], 'html': resp},
         #                      dont_filter=True)
 
-    def parse_dict(self, response, **kwargs):
-        print(f"*******step 2:********\nurl:\t{response.url}")
-        pass
+if __name__ == '__main__':
+    fp = open('../../css/background.css', 'r', encoding='utf-8')
+
+    resp = fp.read()
+    pattern = re.compile(r'\.(?P<tag>.*?)\{background:-(?P<x>.*?)px-(?P<y>.*?)px')
+    result = pattern.finditer(resp)
+    for groups in result:
+        print(groups.group('tag'), groups.group('x'), groups.group('y'))
